579fed436bd63c9363e62f342314c120b2015c0a
WuPoMing <wu178212@gmail.com>
2023-05-12 15:25:09 +0800

    demo

--- demo.py
+++ demo.py
@@ -3,101 +3,91 @@ import pyaudio
 import tensorflow.keras as keras
 import numpy as np
 import librosa
-
+import threading
 import warnings
 warnings.filterwarnings("ignore")
 
-def center_window(root, width, height):
-    screenwidth = root.winfo_screenwidth()
-    screenheight = root.winfo_screenheight()
-    size = f'{width}x{height}+{int((screenwidth - width) / 2)}+{int((screenheight - height) / 2)}'
-    root.geometry(size)
+class Speech_Recognizer:
+    def __init__(self):
+        self.initGUI()
+        self.CHUNK_SIZE = 1024
+        self.SAMPLE_RATE = 48000
+        self.labels = ['speaker1', 'speaker2', 'speaker3', 'speaker4', 'speaker5']
+        self.model = keras.models.load_model('weights/CNN.h5', compile=False)
+        self.stop_recording = False
 
-# Define button to load model
-def load_model():
-    global model
-    global label_speaker
-    model = keras.models.load_model('weights\CNN.h5', compile=False)
-    label_speaker['text'] = 'Model loaded successfully'
+    def initGUI(self):
+        self.root = tk.Tk()
+        self.root.title('Speaker Recognition')
+        self.label_speaker = tk.Label(self.root, text='Press "START" to begin')
+        self.button_start = tk.Button(self.root, text='START', command=threading.Thread(target=self.start).start())
+        self.button_stop = tk.Button(self.root, text='STOP', command=self.stop)
 
-def extract_features(audio_data, sample_rate, n_mfcc=13, n_fft=2048, hop_length=512):
-    # 將音訊資料轉為 MFCC 特徵
-    MFCCs = librosa.feature.mfcc(y=audio_data, sr=sample_rate, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
-    return MFCCs.T.tolist()
-    
-def start():
-    global model
-    global label_speaker
-    global stop_recording
-    CHUNK_SIZE = 1024
-    SAMPLE_RATE = 48000
 
-    # Initialize PyAudio
-    audio = pyaudio.PyAudio()
+    def center_window(self, width, height):
+        screenwidth = self.root.winfo_screenwidth()
+        screenheight = self.root.winfo_screenheight()
+        size = f'{width}x{height}+{int((screenwidth - width) / 2)}+{int((screenheight - height) / 2)}'
+        self.root.geometry(size)
 
-    # Open an input stream
-    stream = audio.open(format=pyaudio.paFloat32, channels=1, rate=SAMPLE_RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+    def extract_features(self, signal, sample_rate, n_mfcc=13, n_fft=2048, hop_length=512):
+        if len(signal) >= sample_rate:
+            # ensure consistency of the length of the signal
+            signal = signal[:sample_rate]
+            # Convert the audio data to MFCC features
+            MFCCs = librosa.feature.mfcc(y=signal, sr=sample_rate, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
+        return np.array(MFCCs.T.tolist())
 
-    # Define the labels for the speakers
-    labels = ['speaker1', 'speaker2', 'speaker3', 'speaker4', 'speaker5']
+    def start(self):
+        # Initialize PyAudio
+        audio = pyaudio.PyAudio()
 
-    stop_recording = False
-    while True:
-        if stop_recording:
-            break
-        # Read audio data from the stream
-        audio_data = stream.read(SAMPLE_RATE)
+        # Open an input stream
+        stream = audio.open(format=pyaudio.paFloat32, channels=1, rate=self.SAMPLE_RATE, input=True, frames_per_buffer=self.CHUNK_SIZE)
 
-        # Convert the audio data to a numpy array
-        audio_samples = np.frombuffer(audio_data, dtype=np.float32)
+        self.stop_recording = False
+        while True:
+            if self.stop_recording:
+                break
+            # Read audio data from the stream
+            audio_data = stream.read(self.SAMPLE_RATE)
 
-        # Extract the MFCC features from the audio data
-        features = extract_features(audio_samples, SAMPLE_RATE)
+            # Convert the audio data to a numpy array
+            audio_samples = np.frombuffer(audio_data, dtype=np.float32)
 
-        # Reshape the features to match the input shape of the model
-        features = np.expand_dims(features, axis=0)
-        
-        # Make a prediction using the model
-        prediction = model.predict(features)
-        predicted_label_index = np.argmax(prediction)
-        predicted_label = labels[predicted_label_index]
+            # Extract the MFCC features from the audio data
+            features = self.extract_features(audio_samples, self.SAMPLE_RATE)
 
-        # Update the label text
-        label_speaker['text'] = 'The speaker is: ' + predicted_label
+            # Reshape the features to match the input shape of the model
+            features = features[np.newaxis, ..., np.newaxis]
 
-        # Update the GUI
-        label_speaker.pack()
-        root.update()
+            # Make a prediction using the model
+            prediction = self.model.predict(features)
+            predicted_label_index = np.argmax(prediction)
+            predicted_label = self.labels[predicted_label_index]
 
-def stop():
-    global stop_recording
-    stop_recording = True
+            # Update the label text
+            self.label_speaker['text'] = 'The speaker is: ' + predicted_label
+
+            # Update the GUI
+            self.label_speaker.pack()
+            self.root.update()
+
+    def stop(self):
+        self.stop_recording = True
+        self.label_speaker['text'] = '暫停'
+
+    def run(self):
+        self.center_window(400, 200)
+
+        # Pack GUI elements
+        self.label_speaker.pack()
+        self.button_start.pack()
+        self.button_stop.pack()
+
+        # Start GUI event loop
+        self.root.mainloop()
 
 if __name__ == '__main__':
-    # Initialize the GUI
-    root = tk.Tk()
-    root.title('Speaker Recognition')
-    root.configure(bg="#FFFFFF")
-    
-    # Define label for speaker prediction
-    label_speaker = tk.Label(root, text='Press "Start" to begin')
-
-    # Define button to load model
-    button_load = tk.Button(root, text='Load Model', command=load_model)
-
-    # Define button to start prediction
-    button_start = tk.Button(root, text='START', command=start)
-
-    # Define button to stop prediction
-    button_stop = tk.Button(root, text='STOP', command=stop)
-
-    center_window(root, 400, 200)
-
-    # Pack GUI elements
-    label_speaker.pack()
-    button_load.pack()
-    button_start.pack()
-    button_stop.pack()
-
-    # Start GUI event loop
-    root.mainloop()
+    app = Speech_Recognizer()
+    app.run()
